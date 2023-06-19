1008558800f3d0e64da51ac2feca2e596d9ec76e
Khamzat Guseynov <91852582+Guseynov1@users.noreply.github.com>
2023-06-19 22:27:25 +0300

    Delete src/main/java/PP312 directory

--- src/main/java/PP312/repo/Repository.java
+++
@@ -1,8 +0,0 @@
-//package PP312.repo;
-//
-//import PP312.model.User;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface Repository extends JpaRepository<User, Long> {
-//
-//}
