59270a0580ed692ce965208d5a181c4c0a9cc9d6
Khamzat Guseynov <91852582+Guseynov1@users.noreply.github.com>
2023-06-19 22:27:18 +0300

    Delete src/main/java/PP315 directory

--- src/main/java/PP315/repositories/RoleJpaRep.java
+++
@@ -1,9 +0,0 @@
-package PP315.repositories;
-
-import PP315.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleJpaRep extends JpaRepository<Role, Long> {
-}
