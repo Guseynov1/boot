9c6df359c23b749b692182ececfa5ebcb198da2b
Khamzat Guseynov <91852582+Guseynov1@users.noreply.github.com>
2023-06-21 16:52:24 +0300

    Update UserDetailService.java

--- src/main/java/PP314/service/UserDetailService.java
+++ src/main/java/PP314/service/UserDetailService.java
@@ -7,7 +7,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 @AllArgsConstructor
 @Component
@@ -15,11 +14,10 @@ public class UserDetailService implements UserDetailsService {
 
     private final UserRepository userRepo;
 
-    @Transactional
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepo.findByUsername(username);
         if (user == null) throw new UsernameNotFoundException("User not found");
-        return new Details(user);
+        return user;
     }
 }
