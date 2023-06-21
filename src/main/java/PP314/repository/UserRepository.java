83d2a5034b50e76f6747939d7090490815ee5971
Khamzat Guseynov <91852582+Guseynov1@users.noreply.github.com>
2023-06-21 16:50:52 +0300

    Delete src/main/java/PP314 directory

--- src/main/java/PP314/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package PP314.repository;
-
-import PP314.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
