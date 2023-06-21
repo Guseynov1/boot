58c52fe273cd626868b50b55e21bbd6876032b46
Khamzat Guseynov <91852582+Guseynov1@users.noreply.github.com>
2023-06-21 16:53:52 +0300

    Update UserDetailService.java

--- src/main/java/PP313/service/UserDetailService.java
+++ src/main/java/PP313/service/UserDetailService.java
@@ -1,25 +1,23 @@
-package PP313.service;
+package PP314.service;
 
-import PP313.model.User;
-import PP313.repo.UserRepo;
+import PP314.model.User;
+import PP314.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 @AllArgsConstructor
 @Component
 public class UserDetailService implements UserDetailsService {
 
-    private final UserRepo userRepo;
+    private final UserRepository userRepo;
 
-    @Transactional
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepo.findByUsername(username);
         if (user == null) throw new UsernameNotFoundException("User not found");
-        return new Details(user);
+        return user;
     }
 }
